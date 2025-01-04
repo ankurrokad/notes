8e0ac5adc32881fc81c026c9dee079fe9029de0f
Ankur Rokad <46367897+ankurrokad@users.noreply.github.com>
2025-01-04 06:59:22 +0530

    Update route.ts

--- src/app/api/authenticate/route.ts
+++ src/app/api/authenticate/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       path: "/",
-      maxAge: 60 * 60 * 24, // 1 day
+      maxAge: 60 * 60 * 24 * 30, // 1 day
     });
 
     const response = NextResponse.json({ success: true });
